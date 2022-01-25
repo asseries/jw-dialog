eb6e98ca8fb54094bbb291befe5f77b34b2c6a96
AdxamSoataliyev <95ajimdc@gmail.com>
2022-01-25 18:37:10 +0500

    Fixed - >>>> theme and two app installation

--- jw-dialog-library/src/main/java/uz/isti/jw_dialog_library/KtUtils.kt
+++ jw-dialog-library/src/main/java/uz/isti/jw_dialog_library/KtUtils.kt
@@ -4,33 +4,37 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 
 object KtUtils {
-        fun Context.showError(message: String){
-            val dialog = JWAlertDialog(JWAlertType.ERROR, message)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showError(message: String) {
+        val dialog = JWAlertDialog(JWAlertType.ERROR, message)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 
-        fun Context.showWarning(message: String){
-            val dialog = JWAlertDialog(JWAlertType.WARNING, message)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showWarning(message: String) {
+        val dialog = JWAlertDialog(JWAlertType.WARNING, message)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 
-        fun Context.showInfo(message: String){
-            val dialog = JWAlertDialog(JWAlertType.INFO, message)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showInfo(message: String) {
+        val dialog = JWAlertDialog(JWAlertType.INFO, message)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 
-        fun Context.showSuccess(message: String){
-            val dialog = JWAlertDialog(JWAlertType.SUCCESS, message)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showSuccess(message: String) {
+        val dialog = JWAlertDialog(JWAlertType.SUCCESS, message)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 
-        fun Context.showJWAlert(type: JWAlertType, message: String, callback: JSAlertDialogCallback? = null){
-            val dialog = JWAlertDialog(type, message, callback)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showJWAlert(
+        type: JWAlertType,
+        message: String,
+        callback: JSAlertDialogCallback? = null
+    ) {
+        val dialog = JWAlertDialog(type, message, callback)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 
-        fun Context.showJWConfirm(message: String, listener: JWConfirmDialogListener? = null){
-            val dialog = JWConfirmDialog(message, listener)
-            dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
-        }
+    fun Context.showJWConfirm(message: String, listener: JWConfirmDialogListener? = null) {
+        val dialog = JWConfirmDialog(message, listener)
+        dialog.show((this as AppCompatActivity).supportFragmentManager, dialog.tag)
+    }
 }
